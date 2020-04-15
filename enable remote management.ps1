5ff4c266024023d1c1e716b735b8985dd873adef
shahjahani <63743048+shahjahani@users.noreply.github.com>
2020-04-15 23:59:43 +0430

    Create enable remote management.ps1

---
+++ enable remote management.ps1
@@ -0,0 +1,9 @@
+Set-NetFirewallRule -DisplayName 'COM+ Network Access (DCOM-In)' –Enabled True -Profile Any
+
+Set-NetFirewallRule -DisplayName 'Remote Event Log Management (RPC)' –Enabled True -Profile Any
+Set-NetFirewallRule -DisplayName 'Remote Event Log Management (NP-In)' –Enabled True -Profile Any
+Set-NetFirewallRule -DisplayName 'Remote Event Log Management (RPC-EPMAP)' –Enabled True -Profile Any
+
+Set-NetFirewallRule -DisplayName 'Remote Volume Management - Virtual Disk Service (RPC)' –Enabled True -Profile Any
+Set-NetFirewallRule -DisplayName 'Remote Volume Management - Virtual Disk Service Loader (RPC)' –Enabled True -Profile Any
+Set-NetFirewallRule -DisplayName 'Remote Volume Management (RPC-EPMAP)' –Enabled True -Profile Any
